98619a99eacc2e39ac3024e55b1ef2e8ed15dd15
Xavier Hainaux <xavier.hainaux@gmail.com>
2020-01-17 22:04:44 +0100

    Setup github actions

---
+++ .github/scripts/install_dart.sh
@@ -0,0 +1,25 @@
+set -e
+
+if [[ -z "$DART_SDK_VERSION" ]]; then
+  DART_SDK_VERSION='2.7.0'
+fi
+
+case "$OSTYPE" in
+  darwin*)  DART_ZIP_NAME="dartsdk-macos-x64-release.zip" ;;
+  linux*)   DART_ZIP_NAME="dartsdk-linux-x64-release.zip" ;;
+  *)        DART_ZIP_NAME="dartsdk-windows-x64-release.zip" ;;
+esac
+
+DART_CHANNEL="stable"
+if [[ $DART_SDK_VERSION == *"-dev."* ]]; then
+  DART_CHANNEL="dev"
+fi
+
+DART_SDK_URL="https://storage.googleapis.com/dart-archive/channels/$DART_CHANNEL/raw/$DART_SDK_VERSION/sdk/$DART_ZIP_NAME"
+
+echo "download $DART_SDK_URL"
+##wget -qO- $DART_SDK_URL | tar -xvzf -
+
+DART_SDK_BIN_PATH=$(pwd)/dart-sdk/bin
+#export PATH=$PATH:$DART_SDK_BIN_PATH
+echo "::add-path::$DART_SDK_BIN_PATH"
